1898222b84108afeb320b43085551ca952ca900e
Dumirtita <dumitrita@gmail.com>
2025-10-01 00:11:10 +0300

    started to implement the wifi mode logic

--- src/WifiManager.cpp
+++ src/WifiManager.cpp
@@ -1,11 +1,20 @@
 #include "WifiManager.h"
 
-IPAddress local_IP(192,168,4,1);
-IPAddress gateway(192,168,4,1);
-IPAddress subnet(255,255,255,0);
+IPAddress local_IP(DEFAULT_IP);
+IPAddress gateway(DEFAULT_GATEWAY);
+IPAddress subnet(DEFAULT_MASK);
+
 
 void WifiManager::Setup() {
     WiFi.softAPConfig(local_IP, gateway, subnet);
     WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+}
 
+void WifiManager::SwitchMode(WifiMode mode) {
+    if (mode == Hotspot) {
+
+    }
+    else if (mode == Client) {
+
+    }
 }
